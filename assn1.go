215de619ee4e8684466e32f12a806d776b64c0af
Nishank <nishanksrj@gmail.com>
2019-08-24 01:40:41 +0530

    InitUser method added

--- assn1.go
+++ assn1.go
@@ -4,7 +4,6 @@ package assn1
 // imports it will break the autograder, and we will be Very Upset.
 //
 import (
-
 	// You neet to add with
 	// go get github.com/sarkarbidya/CS628-assn1/userlib
 
@@ -66,7 +65,7 @@ func someUsefulThings() {
 	userlib.DebugMsg("Key is %v", key)
 }
 
-var configBlockSize = 4096 //Do not modify this variable
+var configBlockSize = 4096  //Do not modify this variable
 
 //setBlockSize - sets the global variable denoting blocksize to the passed parameter. This will be called only once in the beginning of the execution
 func setBlockSize(blocksize int) {
@@ -85,6 +84,9 @@ func bytesToUUID(data []byte) (ret uuid.UUID) {
 //User : User structure used to store the user information
 type User struct {
 	Username string
+	PasswordHash []byte
+	RSAPrivateKey *userlib.PrivateKey
+
 	// You can add other fields here if you want...
 	// Note for JSON to marshal/unmarshal, the fields need to
 	// be public (start with a capital letter)
@@ -95,6 +97,7 @@ type User struct {
 // of data []byte is a multiple of the blocksize; if
 // this is not the case, StoreFile should return an error.
 func (userdata *User) StoreFile(filename string, data []byte) (err error) {
+	return nil
 }
 
 //
@@ -104,7 +107,7 @@ func (userdata *User) StoreFile(filename string, data []byte) (err error) {
 // the block size; if it is not, AppendFile must return an error.
 // AppendFile : Function to append the file
 func (userdata *User) AppendFile(filename string, data []byte) (err error) {
-
+	return nil
 }
 
 // LoadFile :This loads a block from a file in the Datastore.
@@ -116,10 +119,12 @@ func (userdata *User) AppendFile(filename string, data []byte) (err error) {
 // LoadFile is also expected to be efficient. Reading a random block from the
 // file should not fetch more than O(1) blocks from the Datastore.
 func (userdata *User) LoadFile(filename string, offset int) (data []byte, err error) {
+	return nil,nil
 }
 
 // ShareFile : Function used to the share file with other user
 func (userdata *User) ShareFile(filename string, recipient string) (msgid string, err error) {
+	return "ad",nil
 }
 
 // ReceiveFile:Note recipient's filename can be different from the sender's filename.
@@ -128,10 +133,12 @@ func (userdata *User) ShareFile(filename string, recipient string) (msgid string
 // it is authentically from the sender.
 // ReceiveFile : function used to receive the file details from the sender
 func (userdata *User) ReceiveFile(filename string, sender string, msgid string) error {
+	return nil
 }
 
 // RevokeFile : function used revoke the shared file access
 func (userdata *User) RevokeFile(filename string) (err error) {
+	return nil
 }
 
 // This creates a sharing record, which is a key pointing to something
@@ -166,6 +173,46 @@ type sharingRecord struct {
 
 //InitUser : function used to create user
 func InitUser(username string, password string) (userdataptr *User, err error) {
+	// Initialize a new User struct
+	user := User{}
+	// assign user properties to User struct variables
+	user.Username = username
+
+	// generate password hash
+	h := userlib.NewSHA256()
+	h.Write([]byte(password))
+	hashedPassword := string(h.Sum(nil))
+	h = userlib.NewSHA256()
+	h.Write([]byte(hashedPassword + username))
+
+	// set password hash and RSAPrivateKey in User struct
+	user.PasswordHash = h.Sum(nil)
+	user.RSAPrivateKey, err = userlib.GenerateRSAKey()
+
+	// encode User struct into a json object
+	userJson, err := json.Marshal(user)
+	if err!=nil {
+		panic(err)
+	}
+
+	// convert json to string
+	userString := string(userJson)
+	//
+	text := []byte(userString)
+
+	key := userlib.Argon2Key([]byte(password), []byte(username), uint32(userlib.AESKeySize))
+	dataStoreKey := string(userlib.Argon2Key(key, []byte(username), uint32(userlib.AESKeySize)))
+
+	cipherText := make([]byte, userlib.BlockSize + len(text))
+
+	copy(cipherText[:userlib.BlockSize], userlib.RandomBytes(userlib.BlockSize))
+
+	stream := userlib.CFBEncrypter(key, cipherText[:userlib.BlockSize])
+	stream.XORKeyStream(cipherText[userlib.BlockSize:], text)
+
+	userlib.DatastoreSet(dataStoreKey, cipherText)
+
+	return &user,nil
 }
 
 // GetUser : This fetches the user information from the Datastore.  It should
@@ -173,4 +220,5 @@ func InitUser(username string, password string) (userdataptr *User, err error) {
 // data was corrupted, or if the user can't be found.
 //GetUser : function used to get the user details
 func GetUser(username string, password string) (userdataptr *User, err error) {
+	return nil,nil
 }
